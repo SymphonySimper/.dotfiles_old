ef861e30345a682e94abe8344daf33ab035a52fa
SymphonySimper <SymphonySimper@users.noreply.github.com>
2021-07-22 13:14:11 +0530

    change Downloads -> dl; Pictures -> pics; download dir to user.js

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -1,2 +1,3 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.display.background_color", "#111111");
+user_pref("browser.download.dir", "/home/s2b/dl");
