45f1632768cdcaa21e32a9db49cd3bdcf4881b46
SymphonySimper <SymphonySimper@users.noreply.github.com>
2021-07-31 22:28:55 +0530

    changes

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -1,3 +1,3 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.display.background_color", "#000000");
+user_pref("browser.display.background_color", "#ffffff");
 user_pref("browser.download.dir", "/home/s2b/dl");
