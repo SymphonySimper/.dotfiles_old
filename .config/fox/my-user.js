5e8de50acdb8435becad5813e739cdce7c501585
SymphonySimper <SymphonySimper@users.noreply.github.com>
2021-10-09 10:34:08 +0530

    update
    v now opens kvm images
    stuff added to user.js
    gen-fox fix path
    rofi now has pling support
    useragent-switch has bug with youtube so now it is disabled in youtube

--- .config/fox/my-user.js
+++ .config/fox/my-user.js
@@ -1,4 +1,5 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.display.background_color", "#000000");
 user_pref("browser.download.dir", "/home/s2b/dl");
-user_pref("media.hardware-video-decoding.force-enabled", true);
+user_pref("media.hardware-video-decoding.force-enabled", false);
+user_pref("media.hardware-video-decoding.enabled", false);
