061d142411f466136b244704e0ba9ed32d529a5b
SymphonySimper <SymphonySimper@users.noreply.github.com>
2021-10-09 14:15:26 +0530

    update and bug fix

--- .config/fox/my-user.js
+++ .config/fox/my-user.js
@@ -1,5 +1,6 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.display.background_color", "#000000");
 user_pref("browser.download.dir", "/home/s2b/dl");
-user_pref("media.hardware-video-decoding.force-enabled", false);
-user_pref("media.hardware-video-decoding.enabled", false);
+user_pref("media.hardware-video-decoding.force-enabled", true);
+user_pref("media.hardware-video-decoding.enabled", true);
+user_pref("privacy.resistFingerprinting", false);
