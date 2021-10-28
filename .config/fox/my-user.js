bf78438a4f29a7737cd8ef6b21a7ad13e5a12abf
SymphonySimper <SymphonySimper@users.noreply.github.com>
2021-10-28 21:49:30 +0530

    fix dark reader bug with resist set to true and disable webgl

--- .config/fox/my-user.js
+++ .config/fox/my-user.js
@@ -3,4 +3,4 @@ user_pref("browser.display.background_color", "#000000");
 user_pref("browser.download.dir", "/home/s2b/dl");
 user_pref("media.hardware-video-decoding.force-enabled", true);
 user_pref("media.hardware-video-decoding.enabled", true);
-user_pref("privacy.resistFingerprinting", false);
+user_pref("privacy.resistFingerprinting", true);
