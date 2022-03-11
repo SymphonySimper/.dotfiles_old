99874fd565a6ebe808015aa6d2af13a79aa41a2f
SymphonySimper <SymphonySimper@users.noreply.github.com>
2022-03-11 11:41:57 +0530

    alt key no longer opens the menu

--- .config/fox/my-user.js
+++ .config/fox/my-user.js
@@ -5,3 +5,4 @@ user_pref("media.hardware-video-decoding.force-enabled", true);
 user_pref("media.hardware-video-decoding.enabled", true);
 user_pref("privacy.resistFingerprinting", false);
 user_pref("full-screen-api.warning.timeout", 0);
+user_pref("ui.key.menuAccessKeyFocuses", false);
