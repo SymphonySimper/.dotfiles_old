c47af90090b89e0cbe0897e6055c80f4875a0250
SymphonySimper <SymphonySimper@users.noreply.github.com>
2022-04-14 11:17:07 +0530

    new options

--- .config/nvim/lua/my/options.lua
+++ .config/nvim/lua/my/options.lua
@@ -17,12 +17,14 @@ local options = {
 	softtabstop = tab,
 	shiftwidth = tab,
 	wrap = false,
+	colorcolumn = "80",
 	scrolloff = 8,
 	sidescrolloff = 8,
 	termguicolors = true,
 	updatetime = 300,
 	nrformats = "alpha",
 	splitright = true,
+	hls = false,
 }
 
 for k, v in pairs(options) do
