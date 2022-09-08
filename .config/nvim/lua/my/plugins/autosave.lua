e2704863136ef7746df2a88ba112802be5c34223
SymphonySimper <SymphonySimper@users.noreply.github.com>
2022-09-08 14:11:45 +0530

    file omition in autosave

--- .config/nvim/lua/my/plugins/autosave.lua
+++ .config/nvim/lua/my/plugins/autosave.lua
@@ -8,4 +8,19 @@ autosave.setup({
 	trigger_events = { "InsertLeave", "TextChanged" },
 	write_all_buffers = true,
 	debounce_delay = 135,
+	condition = function(buf)
+		local fn = vim.fn
+		local utils = require("auto-save.utils.data")
+
+		if
+			fn.getbufvar(buf, "&modifiable") == 1
+			and utils.not_in(fn.getbufvar(buf, "&filetype"), {})
+			and utils.not_in(fn.expand("%:t"), {
+				"packer.lua",
+			})
+		then
+			return true -- met condition(s), can save
+		end
+		return false -- can't save
+	end,
 })
