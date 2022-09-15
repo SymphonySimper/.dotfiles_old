0eb5560228dc3d419e3e2d08ec0fd69b4af5985d
SymphonySimper <SymphonySimper@users.noreply.github.com>
2022-09-15 14:28:04 +0530

    fix auto-save not loading at startup

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -41,6 +41,35 @@ return {
     end,
   },
 
-  -- remove plugin
-  -- ["hrsh7th/cmp-path"] = false,
+  -- autosave
+  ["Pocco81/auto-save.nvim"] = {
+    event = "VimEnter",
+    module = "auto-save",
+    config = function()
+      require("auto-save").setup {
+        enabled = true,
+        execution_message = {
+          cleaning_interval = 1000,
+        },
+        trigger_events = { "InsertLeave", "TextChanged" },
+        write_all_buffers = true,
+        debounce_delay = 135,
+        condition = function(buf)
+          local fn = vim.fn
+          local utils = require "auto-save.utils.data"
+
+          if
+            fn.getbufvar(buf, "&modifiable") == 1
+            and utils.not_in(fn.getbufvar(buf, "&filetype"), {})
+            and utils.not_in(fn.expand "%:t", {
+              "packer.lua",
+            })
+          then
+            return true -- met condition(s), can save
+          end
+          return false -- can't save
+        end,
+      }
+    end,
+  },
 }
