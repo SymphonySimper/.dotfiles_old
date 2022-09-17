904ee4d67e5576aafeb8d930cc398117879cf311
SymphonySimper <SymphonySimper@users.noreply.github.com>
2022-09-17 21:39:42 +0530

    mv nvim(nvchad) -> nvchad

--- .config/nvim.lua.old/lua/my/plugins/vimwiki.lua
+++
@@ -1,8 +0,0 @@
-vim.g.vimwiki_list = {
-	{
-		path = os.getenv("VIMWIKI"),
-		path_html = os.getenv("XDG_DATA_HOME") .. "/mywiki-html",
-		diary_rel_path = "personal/diary/",
-		auto_diary_index = 1,
-	},
-}
