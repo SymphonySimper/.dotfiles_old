c96cc8e80e16a291766cca29fe5089f519af91db
SymphonySimper <SymphonySimper@users.noreply.github.com>
2023-02-25 17:41:17 +0530

    switch to catppuccin mocha colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,7 +4,22 @@ return {
     lazy = false,
     opts = {
       style = "night",
-      transparent = false,
+      transparent = true,
+    },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      transparent_background = false,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
     },
   },
 }
