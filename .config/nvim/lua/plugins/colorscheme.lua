1cf69450c92088f4edd9a41107e50c47b265bc36
SymphonySimper <SymphonySimper@users.noreply.github.com>
2023-03-06 19:45:39 +0530

    set transparency to false

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    opts = {
-      style = "night",
-      transparent = true,
-    },
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   opts = {
+  --     style = "night",
+  --     transparent = true,
+  --   },
+  -- },
   {
     "catppuccin/nvim",
     lazy = false,
     name = "catppuccin",
     opts = {
       flavour = "mocha",
-      transparent_background = true,
+      transparent_background = false,
     },
   },
   {
