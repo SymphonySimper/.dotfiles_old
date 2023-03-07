4e8a7cc671f27519da4d0d1b805a1ae3e0a33023
SymphonySimper <SymphonySimper@users.noreply.github.com>
2023-03-07 19:11:53 +0530

    add mason config

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,20 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "autopep8",
+      "json-lsp",
+      "lua-language-server",
+      "marksman",
+      "ruff-lsp",
+      "shellcheck",
+      "shfmt",
+      "stylua",
+      "typescript-language-server",
+      "svelte-language-server",
+      "astro-language-server",
+      "rust-analyzer",
+    },
+  },
+}
