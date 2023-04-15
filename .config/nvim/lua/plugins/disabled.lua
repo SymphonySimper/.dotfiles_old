6439abe05a9431d1ad42131c6de90881e1ddd646
SymphonySimper <SymphonySimper@users.noreply.github.com>
2023-04-15 11:10:56 +0530

    disable nvim-notify

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,6 +2,6 @@ return {
   { "ggandor/leap.nvim", enabled = false },
   -- flit depends on leap
   { "ggandor/flit.nvim", enabled = false },
-  -- { "rcarriga/nvim-notify", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
   -- { "folke/noice.nvim", enabled = false },
 }
